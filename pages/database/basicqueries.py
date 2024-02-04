dcd534dfafbbaf3116306968f34a600d966dd348
Mihaela Ilieva <mih1711@abv.bg>
2024-02-04 20:06:17 +0200

    Added questions to the database and some helper functions for connection with it

--- pages/database/basicqueries.py
+++ pages/database/basicqueries.py
@@ -33,3 +33,28 @@ def get_all_users():
     connection.commit()
     connection.close()
     return result
+
+def add_new_question(question, correct_answer, second_option, third_option, fourth_option, difficulty):
+    connection = sqlite3.connect('database/game_portal.db')
+    cursor = connection.cursor()
+    cursor.execute('''INSERT INTO questions(question, correct_answer, second_option, third_option, fourth_option, difficulty) VALUES (?, ?, ?, ?, ?, ?)''', (question, correct_answer, second_option, third_option, fourth_option, difficulty))
+    connection.commit()
+    connection.close()
+
+def get_questions_by_difficulty(difficulty):
+    connection = sqlite3.connect('database/game_portal.db')
+    cursor = connection.cursor()
+    cursor.execute('''SELECT * from questions WHERE difficulty=?''',(difficulty,))
+    result = cursor.fetchall()
+    connection.commit()
+    connection.close()
+    return result
+
+def get_correct_answer(question):
+    connection = sqlite3.connect('database/game_portal.db')
+    cursor = connection.cursor()
+    cursor.execute('''SELECT correct_answer from questions WHERE question=?''',(question,))
+    result = cursor.fetchall()
+    connection.commit()
+    connection.close()
+    return result
