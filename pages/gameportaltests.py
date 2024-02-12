329550210b3a0895bbd48190b8469741e7aecddf
Mihaela Ilieva <mih1711@abv.bg>
2024-02-12 16:29:18 +0200

    Added unit tests for profile page

--- pages/gameportaltests.py
+++ pages/gameportaltests.py
@@ -2,6 +2,7 @@ import unittest
 from tkinter import Tk
 from mainpage import MainPage
 from loginpage import LoginPage
+from profilepage import ProfilePage
 from registerpage import RegisterPage
 from unittest.mock import patch, MagicMock
 
@@ -178,6 +179,68 @@ class TestRegisterPage(unittest.TestCase):
 
         mock_label.return_value.config.assert_called_once_with(text="Please fill in all fields.")
 
+
+class TestProfilePage(unittest.TestCase):
+
+    @patch('profilepage.Tk', spec=Tk)
+    @patch('profilepage.Label')
+    @patch('profilepage.Button')
+    @patch('profilepage.PhotoImage')
+    @patch('profilepage.getrich.GetRichGame')
+    def test_open_get_rich_page(self, mock_getrich_page, mock_photoimage, mock_button, mock_label, mock_tk):
+        mock_tk_instance = MagicMock()
+        mock_tk.return_value = mock_tk_instance
+        
+        profile_page = ProfilePage("Username")
+        profile_page.open_get_rich_page()
+
+        mock_tk_instance.destroy.assert_called_once()
+        mock_getrich_page.assert_called_once()
+
+    @patch('profilepage.Tk', spec=Tk)
+    @patch('profilepage.Label')
+    @patch('profilepage.Button')
+    @patch('profilepage.PhotoImage')  
+    @patch('profilepage.tictactoepage.TicTacToeGame')
+    def test_open_tic_tac_toe_page(self, mock_tictactoe_page, mock_photoimage, mock_button, mock_label, mock_tk):
+        mock_tk_instance = MagicMock()
+        mock_tk.return_value = mock_tk_instance
+        
+        profile_page = ProfilePage("Username")
+        profile_page.open_tic_tac_toe_page()
+
+        mock_tk_instance.destroy.assert_called_once()
+        mock_tictactoe_page.assert_called_once()
+
+    @patch('profilepage.Tk', spec=Tk)
+    @patch('profilepage.Label')
+    @patch('profilepage.Button')
+    @patch('profilepage.PhotoImage')
+    @patch('profilepage.statisticspage.StatisticsDisplay')  
+    def test_open_statistics_page(self, mock_statistics_page, mock_photoimage, mock_button, mock_label, mock_tk):
+        mock_tk_instance = MagicMock()
+        mock_tk.return_value = mock_tk_instance
+        
+        profile_page = ProfilePage("Username")
+        profile_page.open_statistics_page()
+
+        mock_tk_instance.destroy.assert_called_once()
+        mock_statistics_page.assert_called_once()
+
+    @patch('profilepage.Tk', spec=Tk)
+    @patch('profilepage.Label')
+    @patch('profilepage.Button')
+    @patch('profilepage.PhotoImage')  
+    @patch('profilepage.mainpage.MainPage')
+    def test_open_main_page(self, mock_main_page, mock_photoimage, mock_button, mock_label, mock_tk):
+        mock_tk_instance = MagicMock()
+        mock_tk.return_value = mock_tk_instance
+        
+        profile_page = ProfilePage("Username")
+        profile_page.open_main_page()
+
+        mock_tk_instance.destroy.assert_called_once()
+        mock_main_page.assert_called_once()
 
 
 if __name__ == '__main__':
